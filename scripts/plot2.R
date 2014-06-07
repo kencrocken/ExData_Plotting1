c00d1451712f07a1434e43f3c037baa0b1263179
Ken Crocken <kcrocken@gmail.com>
2014-06-07 13:33:17 -0400

    add checks for data dir and data file, if data files do not exists - download and unzip

--- scripts/plot2.R
+++ scripts/plot2.R
@@ -1,5 +1,18 @@
-# --- Read data, assume that data is already present in the working directory
-hh_power <- read.table("household_power_consumption.txt", header = TRUE, 
+# --- Check if data directory exists in working directory, if not create the directory
+if (!file.exists("./data")) {
+    dir.create("./data")
+}
+
+# --- Check if data file exists, if not download data and unzip to data directory
+if (!file.exists("./data/household_power_consumption.txt")) {
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileUrl, destfile = "./data/household_power_consumption.zip", method = "curl")
+    dateDownloaded <- date()
+    unzip("./data/household_power_consumption.zip", exdir = "./data")
+}
+
+# --- Read data
+hh_power <- read.table(".data/household_power_consumption.txt", header = TRUE, 
                        sep = ";", na.strings = "?")
 
 # --- Subset into the appropriate data, coerce the data column into date data type
@@ -13,8 +26,13 @@ hh_power <- rbind(hh_power_01,hh_power_02)
 hh_power$datetime <- paste(hh_power$Date,hh_power$Time, sep = ' ')
 hh_power$datetime <- strptime(hh_power$datetime, format = "%Y-%m-%d %H:%M:%S")
 
+# --- Check if figure directory exists, if not - create the directory
+if (!file.exists("./data/figure")) {
+    dir.create("./data/figure")
+}
+
 # --- plot to PNG file
-png(file = "plot2.png", width = 480, height = 480)
+png(file = "./data/figure/plot2.png", width = 480, height = 480)
 plot (hh_power$datetime,hh_power$Global_active_power, type = "l", xlab = "",
       ylab = "Global Active Power (kilowatts)")
 dev.off()
